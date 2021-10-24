2e286ce429f1aa8be31ad2604c06468de32e4dab
Abdallah Mohammed <abdallahhezam1@gmail.com>
2021-10-24 16:46:15 +0300

    Add files via upload

---
+++ My_Shell/cmake-build-debug/CMakeFiles/My_Shell.cbp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/My_Shell.cbp.dir/main.c.obj"
+  "My_Shell.cbp.exe"
+  "My_Shell.cbp.exe.manifest"
+  "My_Shell.cbp.pdb"
+  "libMy_Shell.cbp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/My_Shell.cbp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
